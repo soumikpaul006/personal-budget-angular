9d4a889bf19088f5745795889389b4c448e72b39
soumikpaul006 <prick5036@gmail.com>
2023-09-17 00:09:09 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 const express=require('express');
 const app=express();
+const fs=require("fs");
 const port=3000;
 
 
 app.use('/', express.static('public'));
 
-
-const budget=
-{
-    myBudget:[
-        {
-            title:'Eat Out',
-            budget:30
-        },
-        {
-            title:'Rent',
-            budget:350
-        },
-        {
-            title:'Groceries',
-            budget:90
-        },
-    ]
-};
 
 app.get('/hello',(req,res )=> {
     res.send('Hello World');
@@ -30,7 +13,21 @@ app.get('/hello',(req,res )=> {
 
 app.get('/budget',(req,res) => {
 
-    res.json(budget);
+    fs.readFile("data.json", "utf-8", (err,jsonData) => {
+
+        if (err) {
+            console.log("Error reading file:", err);
+            return;
+        }
+
+        try {
+            const budgetData = JSON.parse(jsonData);
+            res.json(budgetData);
+
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
 });
 
 app.listen(port, () =>{
